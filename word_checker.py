52c454519b11fdbbce771c9de397861271b315b2
DanielYakubov <Danielyak98@gmail.com>
2022-08-16 21:21:12 -0400

    changed word checker to have assert checks, inherit from ScrabbleSym, and normalized to upper in validation method

--- word_checker.py
+++ word_checker.py
@@ -1,47 +1,16 @@
 import pynini
-
-# dictionary from https://github.com/redbo/scrabble/blob/master/dictionary.txt
-sym_file = 'data/dict.sym'
+from scrabble_sym import ScrabbleSym
 
 
-class WordChecker(object):
+class WordChecker(ScrabbleSym):
     """creates an object that uses a fst symbol table to check if a word is valid in scrabble, and scores it"""
-    letter_to_point_val = {
-        '<BLANK>': 0,
-        'A': 1,
-        'E': 1,
-        'I': 1,
-        'L': 1,
-        'N': 1,
-        'O': 1,
-        'R': 1,
-        'S': 1,
-        'T': 1,
-        'U': 1,
-        'D': 2,
-        'G': 2,
-        'B': 3,
-        'C': 3,
-        'M': 3,
-        'P': 3,
-        'F': 4,
-        'H': 4,
-        'V': 4,
-        'W': 4,
-        'Y': 4,
-        'K': 5,
-        'J': 8,
-        'X': 8,
-        'Q': 10,
-        'Z': 10
-    }
 
     def __init__(self):
-        self.sym = pynini.SymbolTable.read_text(sym_file)
+        super().__init__()
 
     def is_valid_scrabble_word(self, word: str) -> bool:
         """checks if the param word is a valid word in the scrabble dictionary"""
-        return self.sym.member(word)
+        return self.sym.member(word.upper())
 
     @classmethod
     def calculate_base_word_score(cls, word: str) -> int:
@@ -52,5 +21,5 @@ class WordChecker(object):
 if __name__ == '__main__':
     wc = WordChecker()
     word = 'hack'
-    print(wc.is_valid_scrabble_word(word))
-    print(wc.calculate_base_word_score(word))
+    assert wc.is_valid_scrabble_word(word) is True
+    assert wc.calculate_base_word_score(word) == 13
